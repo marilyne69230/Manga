10f67ce4a567a2ff361076bba0d63c3edbeb0419
Marilyne Naili <marilyne.lydia@gmail.com>
2023-09-04 14:25:37 +0200

    add component mangaList et mangaDetails

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { MangaListComponent } from './manga-list/manga-list.component';
+import { MangaDetailsComponent } from './manga-details/manga-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    MangaListComponent,
+    MangaDetailsComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/manga-list/manga-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manga-list',
+  templateUrl: './manga-list.component.html',
+  styleUrls: ['./manga-list.component.css']
+})
+export class MangaListComponent {
+
+}
