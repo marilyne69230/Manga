547a23497040475b6c88a4c78dd759946388fad8
Marilyne Naili <marilyne.lydia@gmail.com>
2023-09-04 14:52:10 +0200

    add mock manga

---
+++ src/app/mock-manga.ts
@@ -0,0 +1,55 @@
+import { Imanga } from './entities';
+
+export const MANGA: Imanga[] = [
+    {
+      "id": 1,
+      "title": "One Piece",
+      "author": "Eiichiro Oda",
+      "description": "Monkey D. Luffy embarks on a journey to become the Pirate King.",
+      "image": "https://example.com/one_piece.jpg"
+    },
+    {
+      "id": 2,
+      "title": "Naruto",
+      "author": "Masashi Kishimoto",
+      "description": "Naruto Uzumaki seeks to become the strongest ninja in his village.",
+      "image": "https://example.com/naruto.jpg"
+    },
+    {
+      "id": 3,
+      "title": "Bleach",
+      "author": "Tite Kubo",
+      "description": "Ichigo Kurosaki gains the powers of a Soul Reaper.",
+      "image": "https://example.com/bleach.jpg"
+    },
+    {
+      "id": 4,
+      "title": "Dragon Ball",
+      "author": "Akira Toriyama",
+      "description": "Goku's adventures to become the strongest fighter in the universe.",
+      "image": "https://example.com/dragon_ball.jpg"
+    },
+    {
+      "id": 5,
+      "title": "Attack on Titan",
+      "author": "Hajime Isayama",
+      "description": "Humanity's fight for survival against giant humanoid creatures.",
+      "image": "https://example.com/attack_on_titan.jpg"
+    },
+    {
+      "id": 6,
+      "title": "Death Note",
+      "author": "Tsugumi Ohba",
+      "description": "A high school student gains a notebook with deadly powers.",
+      "image": "https://example.com/death_note.jpg"
+    },
+    {
+      "id": 7,
+      "title": "Fullmetal Alchemist",
+      "author": "Hiromu Arakawa",
+      "description": "Two brothers seek the Philosopher's Stone to restore their bodies.",
+      "image": "https://example.com/fullmetal_alchemist.jpg"
+    }
+    // Ajoutez d'autres mangas avec leurs liens d'image externes ici
+  ]
+  
