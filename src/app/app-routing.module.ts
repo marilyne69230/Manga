192b57628446393c838313740a2e4189873dd8dd
Marilyne Naili <marilyne.lydia@gmail.com>
2023-09-05 10:08:07 +0200

    add page404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MangaListComponent } from './manga-list/manga-list.component';
+import { Error404Component } from './error404/error404.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'listManga', component: MangaListComponent}
+  {path: 'listManga', component: MangaListComponent},
+  {path: '**', component: Error404Component}
+
 ];
 
 @NgModule({
